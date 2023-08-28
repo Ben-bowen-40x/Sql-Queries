c3fe60a900e4102e9677d42389d409b951b10e7d
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2023-08-28 10:40:54 -0600

    Update Active Customer CTM Query.sql
    This simpler query doesn't exclude any of the phone numbers from the phone2 column

--- CurrentCustomers CTM/Active Customer CTM Query.sql
+++ CurrentCustomers CTM/Active Customer CTM Query.sql
@@ -1,8 +1,4 @@
-SELECT 
-case 
-	when phone1 is not null then phone1
-    when phone2 is not null then phone2
-    end as 'number'
-FROM dwh_reportsdb.customer
-where statusText = 'Active' or status = 1
-;
+select phone1, phone2
+from dwh_reportsdb.customer
+where statusText='Active'
+group by phone1
