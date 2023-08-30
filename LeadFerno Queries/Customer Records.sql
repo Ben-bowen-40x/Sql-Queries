c0a0a0884dd063d4d8ad34a0359061d77800181a
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2023-08-30 11:51:10 -0600

    Create Subscriptions .sql

--- LeadFerno Queries/Customer Records.sql
+++ LeadFerno Queries/Customer Records.sql
@@ -5,6 +5,11 @@ case
 	when c.statusText = 'Active' then 'true'
     else 'false'	
     end as 'status'
+,
+case 
+	when s.initialStatusText = 'Completed' then 'true'
+    else 'false'
+    end as 'serviced'
 from dwh_reportsdb.customer as c
    left join dwh_reportsdb.subscription as s 
    on c.customerID=s.customerID
