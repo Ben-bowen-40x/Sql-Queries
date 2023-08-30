0b7d1bfbb3403f5cedffe054680798bc3e223d41
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2023-08-30 11:51:18 -0600

    Update Call Records.sql

--- LeadFerno Queries/Call Records.sql
+++ LeadFerno Queries/Call Records.sql
@@ -1,11 +1,4 @@
-SELECT contact_number_clean, called_at, sale_billable,
-CASE
-	when sale_billable = 0 then 'False'
-    when sale_billable >0 then 'True'
-    when sale_billable is null then 'False'
-    when sale_billable = '' then 'False'
-    Else 'False'
-    end as 'sale_billable'
+SELECT contact_number_clean, called_at, sale_billable
 FROM dwh_ctmdb.calls
-WHERE called_at like '%2023%'
+WHERE year(called_at) = '2023'
 ;
