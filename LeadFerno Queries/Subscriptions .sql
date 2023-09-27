b14ff08dbba3d53c3d893a98b1672473bb9e44a0
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2023-09-27 15:53:05 -0600

    Update Subscriptions .sql
    Subscription query is finalized

--- LeadFerno Queries/Subscriptions .sql
+++ LeadFerno Queries/Subscriptions .sql
@@ -1,1 +1,4 @@
-select customerID, subscriptionID,dateAdded,
+select s.subscriptionID, s.customerID, s.dateAdded, s.dateCancelled, s.dateReactived
+from dwh_reportsdb.subscription as s
+where year(s.dateAdded) in("2023","2022") #and s.subscriptionID ="899810"
+    
