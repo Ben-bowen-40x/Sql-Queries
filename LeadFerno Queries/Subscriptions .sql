1b6e52d63bbcfc41330891684623ac9c08f57259
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2023-09-29 13:04:18 -0600

    Update Subscriptions .sql
    Bug fix => where statement cannot limit the subscriptions in production

--- LeadFerno Queries/Subscriptions .sql
+++ LeadFerno Queries/Subscriptions .sql
@@ -1,4 +1,3 @@
 select s.subscriptionID, s.customerID, s.dateAdded, s.dateCancelled, s.dateReactived
 from dwh_reportsdb.subscription as s
-where year(s.dateAdded) in("2023","2022") #and s.subscriptionID ="899810"
     
