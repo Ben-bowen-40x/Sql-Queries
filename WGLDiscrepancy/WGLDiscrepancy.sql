682f3fd6ef29398cdfdcb2fe6701ae0d360cdc2d
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2023-10-16 13:37:33 -0600

    Update WGLDiscrepancy.sql

--- WGLDiscrepancy/WGLDiscrepancy.sql
+++ WGLDiscrepancy/WGLDiscrepancy.sql
@@ -1,3 +1,3 @@
 SELECT contact_number_clean, called_at, duration, note, sale_billable, source
 FROM dwh_ctmdb.calls
-WHERE year(called_at) ="2023" and month(called_at) in("07","08","09") and source="WGL"
+WHERE year(called_at) ="2023" and month(called_at) in("08","09") and source="WGL"
