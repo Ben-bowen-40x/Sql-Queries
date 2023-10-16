a1c0322d3d1b92ca44903734e29361e4e373df9d
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2023-10-16 15:13:30 -0600

    Revert "Update Customer Records.sql"
    This reverts commit 324119c97d9691b5a5d7c2f05b64603b2a566da9.

--- LeadFerno Queries/Customer Records.sql
+++ LeadFerno Queries/Customer Records.sql
@@ -1,4 +1,4 @@
-select c.phone1 as "phone1", c.phone2 as "phone2", c.dateAdded, s.contractValue, c.customerID, c.subscriptionIDs, c.dateCancelled, e.fullName
+select c.phone1 as "phone1", c.phone2 as "phone2", c.dateAdded, s.contractValue, c.customerID, c.subscriptionIDs, c.dateCancelled
 ,
 case 
 	when c.statusText = 'Active' then 'true'
@@ -10,6 +10,5 @@ case
     else 'false'
     end as 'serviced'
 from dwh_reportsdb.customer as c
-   left join dwh_reportsdb.subscription as s on c.customerID=s.customerID
-   left join dwh_reportsdb.appointment as a on c.customerID=a.customerID
-   left join dwh_reportsdb.employee as e on a.employeeID=e.employeeID
+   left join dwh_reportsdb.subscription as s 
+   on c.customerID=s.customerID
