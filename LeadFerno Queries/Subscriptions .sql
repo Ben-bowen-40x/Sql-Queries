5734af56237d2f2bbd0544443404c2ba2b28cdad
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2023-10-17 08:18:31 -0600

    Trying to add Rep Names

--- LeadFerno Queries/Subscriptions .sql
+++ LeadFerno Queries/Subscriptions .sql
@@ -1,3 +1,15 @@
-select s.subscriptionID, s.customerID, s.dateAdded, s.dateCancelled, s.dateReactived
+select s.subscriptionID, s.customerID, s.dateAdded, s.dateCancelled, s.dateReactived, e.fullName,
+(
+	Select e.fullName 
+		from dwh_reportsdb.subscription as s
+        left join dwh_reportsdb.employee as e on e.employeeID=s.soldBy2
+) as "SoldBy2",
+(
+	Select e.fullName 
+		from dwh_reportsdb.subscription as s
+        left join dwh_reportsdb.employee as e on e.employeeID=s.soldBy3
+) as "SoldBy3"
 from dwh_reportsdb.subscription as s
+left join dwh_reportsdb.employee as e on e.employeeID=s.soldBy
+
     
