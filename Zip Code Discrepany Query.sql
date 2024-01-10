be32f4fd7981d3b0ac58d7d5b46fbd239e576f61
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2024-01-10 11:16:08 -0700

    Delete Zip Code Discrepany Query.sql

--- Zip Code Discrepany Query.sql
+++
@@ -1,4 +0,0 @@
-SELECT contact_number_clean, called_at, dateContacted, note, zip
-FROM dwh_ctmdb.calls
-WHERE called_at BETWEEN '2023-01-01' AND '2023-08-31'
-AND (zip = "23608" OR zip = "06880" OR zip = "02562" OR zip = "78584" OR zip = "32804" OR zip = "32811")
