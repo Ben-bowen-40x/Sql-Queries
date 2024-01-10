cbc7a1609f5f6c5adb800fc2cfda758be8022497
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2024-01-10 11:15:21 -0700

    Delete Subscriptions .sql

--- LeadFerno Queries/Subscriptions .sql
+++
@@ -1,5 +0,0 @@
-select s.subscriptionID, s.customerID, s.dateAdded, s.dateCancelled, s.dateReactived, e.fullName, f.fullName as "SoldBy2", g.fullName as "SoldBy3"
-from dwh_reportsdb.subscription as s
-left join dwh_reportsdb.employee as e on e.employeeID=s.soldBy
-left join dwh_reportsdb.employee as f on f.employeeID=s.soldBy2
-left join dwh_reportsdb.employee as g on g.employeeID=s.soldBy3
