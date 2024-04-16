9ae382313d7d307c22fc48f37d4025177d4391f3
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2024-04-16 17:28:37 -0600

    Update WGLDiscrepancy.sql

--- WGLDiscrepancy/WGLDiscrepancy.sql
+++ WGLDiscrepancy/WGLDiscrepancy.sql
@@ -1,4 +1,8 @@
-SELECT contact_number_clean, called_at, duration, note, sale_billable, source
+SELECT contact_number_clean, called_at, duration, note, source,
+case
+	when sale_billable = 'billable' then 'true'
+    else 'false'
+    end as 'billable'
 FROM dwh_ctmdb.calls
-WHERE dateContacted between "2023-10-01" and CURDATE() and source = "WGL" 
+WHERE dateContacted between '2023-10-01' and CURDATE() and source = 'WGL'
 ;
