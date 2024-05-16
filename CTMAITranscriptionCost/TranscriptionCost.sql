5f4803a20ec8628f8903e7b7dc6ed48882566c01
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2024-05-16 12:21:17 -0600

    Delete TranscriptionCost.sql

--- CTMAITranscriptionCost/TranscriptionCost.sql
+++
@@ -1,3 +0,0 @@
-select year(dateContacted) as Year, sum(duration)/60*.02 as High, sum(if(duration>=300,1,0))*5*.02 as fiveMinuteMax, sum(if(duration<300,duration,0))/60*.02 lessThanFive
-from dwh_ctmdb.calls
-group by year(dateContacted)
