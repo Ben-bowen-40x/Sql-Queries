4b2763a7784dda11abf7a2dd84ecf44575d14910
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2024-05-16 12:21:20 -0600

    Create Spam from Five9 only.sql

---
+++ Spam--CTM-Five9/Spam from Five9 only.sql
@@ -0,0 +1,7 @@
+select ani, disposition, campaign, timestamp
+from dwh_five9db.calls
+where disposition like "%spam%" 
+and campaign in("Inside Sales", "Sales Spanish") 
+and campaignType = "Inbound"
+and month(timestamp) = 1 and year(timestamp) = 2024 and day(timestamp) in (1,2,3,4,5,6,7)
+group by ani
